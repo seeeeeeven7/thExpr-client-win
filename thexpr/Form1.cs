281565c65823112cc5ce16207ea9ca25e187dfdf
胡天翔 <seeeeeeven7@gmail.com>
2016-12-15 14:52:45 +0800

    Initial Commit

---
+++ thexpr/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Windows.Forms;
+using thexpr.Utility;
+
+namespace thexpr
+{
+    public partial class Form1 : Form
+    {
+        private ClipBoardMonitor clipBoardMonitor = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            clipBoardMonitor = new ClipBoardMonitor();
+            clipBoardMonitor.NewFileDropList += clipBoardMonitor_NewFileDropList;
+        }
+
+        private void clipBoardMonitor_NewFileDropList(StringCollection fileDropList)
+        {
+            if (fileDropList.Count != 0)
+            {
+                Console.WriteLine(fileDropList[0]);
+                pictureBox1.Image = Image.FromFile(fileDropList[0]);
+            }
+        }
+    }
+}
+
+
---
+++ thexpr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using thexpr.Views;
+
+namespace thexpr
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Application.Run(new LoginForm());
+        }
+    }
+}
